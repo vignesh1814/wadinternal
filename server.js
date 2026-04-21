6efdc05d57ab61ad75b2607d0cf970d865b53ff0
vignesh1814 <theerdalavignesh@gmail.com>
2026-04-21 10:24:57 +0530

    sdsds

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const http = require(http);
-const os = require(os);
-const path = require(path);
-const eventEmitter = require(events);
+//Create a node server using http ,os,path and events module
+const http = require('http');
+const os = require('os');
+const path = require('path');
+const EventEmitter = require('events');
 
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('request_received', (url) => {
+    console.log(`Request received for: ${url}`);
+});
+
+eventEmitter.on('event_page_visited', (time) => {
+    console.log('event page visited at ${time}');
+});
 
